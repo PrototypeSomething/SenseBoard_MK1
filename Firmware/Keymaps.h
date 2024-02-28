2b1296b634c6e8aca07700dbfd70e3537cb4e3bc
Lucas Bäckdahl <46003477+PrototypeSomething@users.noreply.github.com>
2024-02-28 23:33:35 +0100

    Added keymaps

--- Firmware/Keymaps.h
+++ Firmware/Keymaps.h
@@ -20,12 +20,12 @@ int muxChannel;
   \-------+-------+-------+-------------------------------------+-----+-----+-----+-----+-----+-----/
 */
 
-const char keymaps[numberOfMuxes][muxChannels] = {
+const int keymaps[numberOfMuxes][muxChannels] = {
   { KC_LGUI, KC_LCTRL, KC_LSHIFT, KC_CAPSLOCK, KC_TAB, KC_GRAVE, KC_ESCAPE, KC_F1, KC_1, KC_Q, KC_A, KC_Z, KC_LALT, KC_X, KC_S, KC_W },
   { KC_2, KC_F2, KC_F3, KC_3, KC_E, KC_D, KC_C, KC_V, KC_F, KC_R, KC_4, KC_F4, KC_F5, KC_5, KC_T, KC_G },
   { KC_SPACE, KC_B, KC_N, KC_H, KC_Y, KC_6, KC_F6, KC_F7, KC_7, KC_U, KC_J, KC_M, KC_K, KC_I, KC_8, KC_F8 },
-  { KC_F9, KC_9, KC_O, KC_L },
-  { KC_RETURN }
+  { KC_F9, KC_9, KC_O, KC_L, KC_COMMA, KC_DOT, KC_RALT, KC_MENU, KC_SCOLON, KC_P, KC_0, KC_F10, KC_F11, KC_MINUS, KC_LBRACKET, KC_QUOTE },
+  { KC_ENTER, KC_SLASH, KC_RCTRL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RSHIFT, KC_UP, KC_RBRACKET, KC_PLUS, KC_F12, KC_BSPACE, KC_BSLASH, KC_PSCREEN, KC_DELETE, KC_DELETE }
   /*{KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_LEFT_SHIFT, KEY_CAPS_LOCK, KEY_TAB, 0x7e, KEY_ESC, KEY_F1, '1', 'q', 'a', 'z', KEY_LEFT_ALT, 'x', 's', 'w'},    //  Used for another library (BLE52_Mouse_and_Keyboard.h)
   {'2', KEY_F2, KEY_F3, '3', 'e', 'd', 'c', 'v', 'f', 'r', '4', KEY_F4, KEY_F5, '5', 't', 'g'},
   {' ', 'b', 'n', 'h', 'y', '6', KEY_F6, KEY_F7, '7', 'u', 'j', 'm', 'k', 'i', '8', KEY_F8},
